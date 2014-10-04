769b5cac63bc59245365fc694f5f325e852ac976
Kyle Mathews <mathews.kyle@gmail.com>
2014-10-04 16:08:15 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 /**
- * Copyright Marc J. Schmidt. See the LICENSE file at the top-level
- * directory of this distribution and at
- * https://github.com/marcj/css-element-queries/blob/master/LICENSE.
+ * Copyright Marc J. Schmidt
  */
 /**
  * Class for dimension change detection.
