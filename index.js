24238f940703f5c84e404d15b185d60c25d1460a
mariusw <marius.wirtherle@locom.de>
2016-07-12 15:26:55 +0200

    add style opacity: 0; to fix issue #9

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ var exports = function exports(element, fn) {
         element.style.position = 'relative'
       }
       var obj = element.__resizeTrigger__ = document.createElement('object')
-      obj.setAttribute('style', 'display: block; position: absolute; top: 0; left: 0; height: 100%; width: 100%; overflow: hidden; pointer-events: none; z-index: -1;')
+      obj.setAttribute('style', 'display: block; position: absolute; top: 0; left: 0; height: 100%; width: 100%; overflow: hidden; pointer-events: none; z-index: -1; opacity: 0;')
       obj.setAttribute('class', 'resize-sensor')
       obj.__resizeElement__ = element
       obj.onload = objectLoad
