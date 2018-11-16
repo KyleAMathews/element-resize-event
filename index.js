4bed032e6d70aa792bcca1f2a2eb77ef90a231f1
Viktor Tiurin <vtiurin@spotware.com>
2018-11-16 15:37:13 +0200

    Partially implement code from https://github.com/KyleAMathews/element-resize-event/pull/31 (see discussion)

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ var exports = function exports(element, fn) {
       var obj = (element.__resizeTrigger__ = document.createElement('object'))
       obj.setAttribute(
         'style',
-        'display: block; position: absolute; top: 0; left: 0; height: 100%; width: 100%; overflow: hidden; pointer-events: none; z-index: -1; opacity: 0;'
+        'position: absolute; top: 0; left: 0; height: 100%; width: 100%; pointer-events: none; z-index: -1; opacity: 0;'
       )
       obj.setAttribute('class', 'resize-sensor')
 
